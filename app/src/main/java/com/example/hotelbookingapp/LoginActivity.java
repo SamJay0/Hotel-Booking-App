a9b80adebd863d6987be121a71ce648b586da89e
SamJay0 <samjay0.254@gmail.com>
2019-05-28 20:54:52 +0300

    designed sign_up page

--- app/src/main/java/com/example/hotelbookingapp/LoginActivity.java
+++ app/src/main/java/com/example/hotelbookingapp/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.hotelbookingapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login=findViewById(R.id.login_btn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "login", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+    public void sign_up(View view){
+        Intent i=new Intent(this,SignUpActivity.class);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/example/hotelbookingapp/SignUpActivity.java
+++ app/src/main/java/com/example/hotelbookingapp/SignUpActivity.java
@@ -2,6 +2,9 @@ package com.example.hotelbookingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Button signup=findViewById(R.id._sign_up_btn);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignUpActivity.this, "sign up", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
